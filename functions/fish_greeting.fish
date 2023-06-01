59a9ac6d1a15c117a125cbd696c7b882956bf37f
grscheller <geoffrey@scheller.com>
2023-06-01 03:57:33 -0600

    Updated fish_greeting.fish
    - from displaying nothing
    - to displaying neofetch if available

--- functions/fish_greeting.fish
+++ functions/fish_greeting.fish
@@ -1,2 +1,4 @@
-function fish_greating --description 'Do nothing, suppresses greating message'
+function fish_greeting --description 'Do nothing, suppresses greating message'
+   test -x /usr/bin/neofetch
+   and /usr/bin/neofetch
 end
