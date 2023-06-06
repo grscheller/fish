122d8cb6d44f70aee85db9c92d4788720b96543b
grscheller <geoffrey@scheller.com>
2023-06-05 23:38:04 -0600

    New version functions/ud.fish
    - ud stands for "up dir"
    - takes 0 or 1 arguments
    - for 0 arguments, cd ..
    - for 1 argument
      - if arg decimal integer > 0, jump up that number of directories
      - if arg a string, look for an exact match, then first inital match
        - go to arg* if a dir in a higher directory
        - otherwise go to parent dir of arg* if in a higher directory
        - follows symlinks
      - report if arg* not found in any higher directory
      - report if cd to arg* or parent of arg* fails
    - edge cases
      - ud '' <- takes you to /
      - ud /  <- does not take you to /

--- functions/ud.fish
+++ functions/ud.fish
@@ -1,44 +1,78 @@
 function ud --description 'Jump up multiple directories, default is 1'
-    set -l upDir ..
-    set -l findTarget ''
-    set -l nDirs "$argv[1]"
-    set -l maxUp (math (string split / (pwd) | count) - 1)
+   set -l upDir ..
+   set -l maxUp (math (string split / (pwd) | count) - 1)
+   set -l nDirs
+   set -l target ''
 
-    if string match -qr -- '^[1-9]\d*$' $nDirs
-        # if argument is a number, jump up that number of directories
-        if [ $nDirs -gt $maxUp ]
-            set nDirs $maxUp
-        end
-        while [ $nDirs -gt 1 ]
-            set upDir ../$upDir
-            set nDirs (math $nDirs - 1)
-        end
-    else
-        # look for argument in one of the parent directories
-        set findTarget $nDirs
-        if [ -n "$findTarget" ]
-            set cnt 1
-            while [ $cnt -le $maxUp ]
-                test -e $upDir/$findTarget
-                and break
-                set upDir ../$upDir
-                set cnt (math $cnt + 1)
-            end
-        end
-    end
+   set -l argc (count $argv)
 
-    set -l target "$upDir/$findTarget"
-    if [ ! -e "$target" ]
-        # let user know if target not found in any parent directory
-        printf '"%s" not found in any parent directory\n' $findTarget >&2
-        return 2
-    else if [ -d $target ]
-        # drop into target if a directory
-        cd "$target"
-    else
-        # otherwise go directly to parent directory of target
-        cd $upDir
-    end
+   if test $argc -eq 0
+      cd ..
+      return $status
+   end
+      
+   if test (count $argv) -gt 1 
+      printf 'ud: ud only takes 0 or 1 arguments' >&2
+      return 2
+   end
 
-    return 0
+   if string match -qr -- '^[1-9]\d*$' $argv[1]
+      # If argument is a number, jump up that number number of directories
+      set nDirs $argv[1]
+      test $nDirs -gt $maxUp && set nDirs $maxUp
+   else
+      # Otherwise set nDirs to $maxUp and set target to first argument.
+      set nDirs $maxUp
+      set target $argv[1]
+   end
+
+   set -l cnt 1
+   if test -z $target
+      # Setup number of directories to jump if no target given
+      while test $cnt -lt $nDirs
+         set upDir ../$upDir
+         set cnt (math $cnt + 1)
+      end
+   else
+      # First look for exact match.
+      while test $cnt -le $nDirs
+         test -e $upDir/$target && break
+         set upDir ../$upDir
+         set cnt (math $cnt + 1)
+      end
+      # Next look for leading string match, select first one found.
+      set cnt 1
+      set upDir ..
+      set -l targetsFound
+      set -l upDirPat
+      while test $cnt -le $maxUp
+         if test (count $upDir/$target*) -gt 0
+            set targetsFound $upDir/$target*
+            set target $targetsFound[1]
+            set upDirPat (string replace -a '.' '\\.' $upDir)
+            set target (string replace -r $upDirPat/ '' $target)
+            break
+         end
+         set upDir ../$upDir
+         set cnt (math $cnt + 1)
+      end
+   end
+
+   set -l destination
+   set -l targetDest $upDir/$target
+   if test ! -e $targetDest
+      printf 'ud: "%s" not found in any higher directory\n' $target >&2
+      return 1
+   else if test -d $targetDest
+      set destination $targetDest
+   else
+      set destination $upDir
+   end
+
+   if cd $destination 2>/dev/null
+      return 0
+   else
+      printf 'ud: failed to cd to "%s"\n    for target "%s"\n' $destination $target
+      return 3
+   end
 end
